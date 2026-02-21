f6a6e871590d7837b3d3150e4481cb6543f9a548
GEONHEE KIM <kim12312320@gmail.com>
2026-02-21 17:07:19 +0900

    feat: connect server setup

---
+++ api/[...all].ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { registerOAuthRoutes } from "../server/_core/oauth";
+import { createContext } from "../server/_core/context";
+import { appRouter } from "../server/routers";
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+registerOAuthRoutes(app);
+
+app.use(
+  "/api/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+export default app;
